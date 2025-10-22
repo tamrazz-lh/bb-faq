8b5c515aeb49fe340be0c6b410f8397c74f316b0
Georgy Tamrazyan <gmtamrazyan.lifehacker@gmail.com>
2025-10-22 20:07:11 +0400

    Initial commit

---
+++ BbFaq.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+Plugin Name: [ЛХ/БХ] FAQ
+Description:  [bb-faq] - Добавляет Гутенберг Блок Вопросы-Ответы. Формирует разметку JSON-LD со структурированными данными FAQPage.
+Author: ООО "Буферная бухта"
+Author URI:  https://lifehacker.ru/
+Requires PHP: 8.1
+Version: 1.0.0
+*/
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+define('BB_FAQ__PLUGIN_FILE', __FILE__);
+define('BB_FAQ__PLUGIN_PATH', plugin_dir_path(BB_FAQ__PLUGIN_FILE));
+define('BB_FAQ__PLUGIN_URL', plugin_dir_url(BB_FAQ__PLUGIN_FILE));
+
+require trailingslashit(BB_FAQ__PLUGIN_PATH) . '/vendor/autoload.php';
+
+function bb_faq__init()
+{
+}
+
+add_action('plugins_loaded', 'bb_faq__init', 10);
